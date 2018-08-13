4b846517402712160aa71fb3a00ebe3593fb487c
artmaxcomp@hotmail.com <artmaxcomp@hotmail.com>
2018-08-13 22:11:01 +0300

    EOT command was add

--- index.php
+++ index.php
@@ -3,4 +3,11 @@
 
  print "I am Maks";
 
+ echo <<<EOT
+I read the next slide and now using the EOT command.
+So that's quite easier to create new lines.
+But why dosn't work it any browsers?
+EOT;
+
+
 die('Application end');
